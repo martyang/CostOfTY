b66f9667cd3d08aa3b248662a5fd0419680f650a
min.yang <min.yang@C1905.tinno.com>
2018-06-25 17:38:37 +0800

    11

--- src/CostCalcuation.java
+++ src/CostCalcuation.java
@@ -2,15 +2,10 @@
 public class CostCalcuation {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
 		ChoiceFrame mainFrame = new ChoiceFrame();
 		mainFrame.showFrame();
 		
-=======
-		// TODO Auto-generated method stub
-
->>>>>>> branch 'master' of https://github.com/martyang/CostOfTY.git
 	}
 
 }
