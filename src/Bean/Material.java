204500769a6689acbd4dfe27fcf98259e7226b03
min.yang <min.yang@C1905.tinno.com>
2018-06-26 18:10:02 +0800

    创建物料bean

---
+++ src/Bean/Material.java
@@ -0,0 +1,35 @@
+package Bean;
+
+public class Material {
+	String sn;
+	String name;
+	float number;
+	float cost;
+	
+	public String getSn() {
+		return sn;
+	}
+	public void setSn(String sn) {
+		this.sn = sn;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getNumber() {
+		return number;
+	}
+	public void setNumber(float number) {
+		this.number = number;
+	}
+	public float getCost() {
+		return cost;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+	
+
+}
